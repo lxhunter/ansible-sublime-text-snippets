3b218f7ac703cda7159d834873a2d9ca9e468880
Alexander Jaeger <alexander.jaeger@me.com>
2016-07-10 16:03:07 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# ansible-sublime-text-snippets
-Ansible-doc autogenerates snippets for Sublime Text for auto completion
+# Description
+
+ansible-doc generates snippets for Sublime Text auto completion
+
+## Requirements
+
+- Sublime Text 2/3+
+- gsed for OSX
+- ansible
+- OSX
+
+## Installation
+
+```
+# Clone the repo
+$ git clone https://github.com/lxhunter/ansible-sublime-text-snippets.git
+# Move into the directory
+$ cd ansible-sublime-text-snippets
+# Create the directory which holds the snippets
+$ mkdir ~/Library/Application Support/Sublime Text 3/Packages/ansible-sublime-text-snippets
+# Optional: install coreutils to have gnu sed 
+$ brew install coreutils
+# Optional: install ansible
+$ brew install ansible
+# Optional: make the generator executable
+$ chmod 0755 ansible-doc-to-sublime-snippet.sh
+# Execute the generator
+$ ./ansible-doc-to-sublime-snippet.sh
+```
+
+## Uninstallation
+
+```
+$ rm -r ~/Library/Application Support/Sublime Text 3/Packages/ansible-sublime-text-snippets
+```
+
+## Examples
+
+1. Create a .yaml file in Sublime or use the supplied test.yml
+2. write ```apt``` and hit tab to witness auto completion 
+3. Enjoy!
+
+## Todos
+
+* Use native sed, to requirements 
+* Use different plugin folder to make it os agnostic
+* Package it for plugin Manager
+* My war with search and replace could be less ugly (suggestion welcome)
+
+## Quote
+
+"Nothing will work unless you do." 
+- Maya Angelou
+
+## Credits
+
+[Initial Idea by João Paulo Seregatte](https://github.com/seregatte/AnsibleSnippets)
+
+## Contribute
+
+[Tutorial](http://kbroman.github.io/github_tutorial/pages/fork.html)
+
+## Author
+
+Author:: [Alexander Jäger](https://github.com/lxhunter)
+
+Copyright 2016
---
+++ ansible-doc-to-sublime-snippet.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Reset
+color_off='\033[0m'       # Text Reset
+
+# Regular Colors
+black='\033[0;30m'        # Black
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
+
+target_path="$HOME/Library/Application Support/Sublime Text 3/Packages/ansible-sublime-text-snippets"
+
+if [ ! -d "$target_path" ]; then
+    echo -e >&2 "${red} Target Directory is missing: $target_path${color_off}"
+    exit 1
+fi
+
+command -v gsed >/dev/null || { echo -e >&2 "${red}I need gsed, brew install coreutils${color_off}"; exit 2; }
+command -v ansible-doc >/dev/null || { echo -e >&2 "${red}I need ansible-doc, brew install ansible${color_off}"; exit 3; }
+
+modules=$( ansible-doc -l | cut -d' ' -f1 )
+#modules=( 'apt' )
+
+for module in $modules; do
+
+  if [ "$module" = "DEPRECATED:" ]; then
+      echo "Deprecated modules will be left out!"
+      exit 0
+  fi
+
+  echo -e -n "Generating snippet for ${yellow}'$module'${color_off} module: "
+
+  snippet_content=`ansible-doc -s $module | gsed -e "s/\s\{31\}/FOO/g" | gsed ':a;N;$!ba;s/\n/BAR/g' | gsed -e 's/BARFOO//g' | gsed -e 's/BAR/\n/g' | gsed -e 's/action: \(.*\)/\1:/g' | gsed -e 's/name: \(.*\)/name: \${1:\1}/g' | gsed -e "s/^\s\{6\}\([0-9a-z_]\{1,\}\)=\{,1\}\s\{1,\}#\(.*\)/    # \1: \2/g"`   
+  
+  if [ "$snippet_content" = "" ]; then
+    echo -e "${red} failed! ${color_off}"
+    continue
+  fi 
+
+    echo "<snippet>
+  <content>
+    <![CDATA[$snippet_content]]>
+  </content>
+  <tabTrigger>$module</tabTrigger>
+  <scope>source.yaml,source.ansible</scope>
+</snippet>" > "$target_path/$module.sublime-snippet"
+
+echo -e "${green} done ${color_off}"
+
+done
